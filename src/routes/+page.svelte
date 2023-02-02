6354786e2c71c291eb46286ea8985fc3a900d690
Kate Kim <nk467@cornell.edu>
2023-02-02 17:49:19 -0500

    add modal

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,7 @@
 <script>
-    import { currentUser } from '$lib/pocketbase'
+    import { currentUser } from '$lib/pocketbase';
+
+    let caption = '';
 </script>
 
 {#if $currentUser}
@@ -11,7 +13,17 @@
     <input type="checkbox" id="my-modal-4" class="modal-toggle" />
     <label for="my-modal-4" class="modal cursor-pointer">
         <label class="modal-box relative" for="">
+        <div class="space-y-4">
             <h3 class="text-lg font-bold">Create post</h3>
+            <div class="form-control">
+                <label class="label">
+                    <span class="label-text">Caption</span>
+                </label>
+                <textarea class="textarea textarea-bordered h-24" bind:value={caption}></textarea>
+            </div>
+                <input type="file" class="file-input file-input-bordered w-full max-w-xs" />
+                <button class="btn btn-block btn-primary">Create post</button>
+        </div>
         </label>
     </label>
     </div>
